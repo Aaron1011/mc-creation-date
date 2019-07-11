df11ea022c1574c5c5eb5b01013c160aa5c4ddad
Aaron Hill <aa1ronham@gmail.com>
2019-07-10 23:53:33 -0400

    Get deploy script working

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ set -xe
 
 cp target/x86_64-unknown-linux-musl/release/openwhisk exec
 zip action.zip exec
-wsk action create mc_creation_date action.zip --native
+wsk action update mc_creation_date action.zip --native --web true
